503183d3eb564ebce2f598077f87b3680be6118b
Graham Cole <graham@sgcole.uk>
2024-05-18 08:10:37 +0100

    just commiting

--- terraform/modules/lambda/locals.tf
+++ terraform/modules/lambda/locals.tf
@@ -6,10 +6,3 @@ locals {
   archive_path = "${path.module}/tf_generated/${var.function_name}.zip"
 }
 
-output "lambda_test_binary_path" {
-  value = local.binary_path
-}
-
-output "lambda_name" {
-  value = aws_lambda_function.function.function_name
-}
